059833f3ea8cdd7588ec66c572a4da09bc37e939
Dmitry Gubitskiy <d.gubitskiy@gmail.com>
2015-06-16 16:24:03 +0300

    Can define converters

--- lib/ptero_virtus.rb
+++ lib/ptero_virtus.rb
@@ -4,7 +4,12 @@ module PteroVirtus
     klass.extend(PteroVirtus::ClassMethods)
     klass.include(PteroVirtus::InstanceMethods)
   end
+
+  def self.define_converter(type, &block)
+    PteroVirtus::TypeConverter.define_converter(type, block)
+  end
 end
 
 require_relative 'ptero_virtus/class_methods'
 require_relative 'ptero_virtus/instance_methods'
+require_relative 'ptero_virtus/type_converter'
--- lib/ptero_virtus/type_converter.rb
+++ lib/ptero_virtus/type_converter.rb
@@ -1,12 +1,22 @@
 class PteroVirtus::TypeConverter
 
+  def self.define_converter(type, block)
+    method_name = conversion_method_name_for(type)
+    raise "Converter for #{type} already exists" if instance_methods.include?(method_name)
+    define_method(method_name, block)
+  end
+
+  def self.conversion_method_name_for(type)
+    "convert_to_#{type.to_s.downcase}".to_sym
+  end
+
   def initialize(type, value)
     @type = type
     @value = value
   end
 
   def convert
-    method_name = "convert_to_#{type.to_s.downcase}"
+    method_name = self.class.conversion_method_name_for(type)
     raise "Type not supported: #{type}" unless respond_to?(method_name)
     send(method_name)
   end
@@ -25,6 +35,10 @@ class PteroVirtus::TypeConverter
 
   def convert_to_symbol
     value.to_sym rescue nil
+  end
+
+  def convert_to_boolean
+    !!value
   end
 
   private
